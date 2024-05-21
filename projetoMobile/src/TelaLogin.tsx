d2a59b3269f172eb74c4a0edc671fb3e9c7aaf90
Cauasm1 <cauam2498@gmail.com>
2024-05-20 22:28:14 -0300

    Projeto mobile

--- projetoMobile/src/TelaLogin.tsx
+++ projetoMobile/src/TelaLogin.tsx
@@ -1,45 +1,126 @@
 import React, { useState } from 'react';
-import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable, Image, Alert } from 'react-native';
 
-const TelaLogin = () => {
+import auth from "@react-native-firebase/auth";
+
+const Login = () => {
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
 
-    function log() {
-        console.log('Email:' + email + '\nSenha: ' + senha)
+    function logar() {
+        if (verificaCampos()) {
+
+            auth()
+                .signInWithEmailAndPassword(email, senha)
+                .then(() => { Alert.alert('Logado com sucesso') })
+                .catch((error) => tratarErros(String(error)))
+        }
+    }
+
+    function verificaCampos() {
+        if (email == '') {
+            Alert.alert("Email em branco", "Digite um email")
+            return false;
+        }
+        if (senha == '') {
+            Alert.alert("Senha em branco", "Digite uma senha")
+            return false;
+        }
+
+        return true;
+    }
+
+    function tratarErros(erro: string) {
+        console.log(erro);
+        if (erro.includes("[auth/invalid-email]")) {
+            Alert.alert("Email inválido", "Digite um email válido")
+        } else if (erro.includes("[ INVALID_LOGIN_CREDENTIALS ]")) {
+            Alert.alert("Login ou senha incorretos", "")
+        } else if (erro.includes("[auth/invalid-credential]")) {
+            Alert.alert("Login ou senha incorretos", "")
+        } else {
+            Alert.alert("Erro", erro)
+        }
     }
 
     return (
-        <>
-            <View style={styles.container}>
-                <Image source={{ uri: 'https:cdn-icons-png.flaticon.com/512/4675/4675159.png' }} style={styles.imagem} />
-                <TextInput placeholder='Digite seu email' />
-                <TextInput placeholder='Digite sua senha' />
+        <View style={styles.container}>
+            <View style={styles.painel_imagem}>
+                <Image
+                    style={styles.imagem}
+                    source={{ uri: 'https:cdn-icons-png.flaticon.com/512/4675/4675159.png' }} />
             </View>
 
-            <Text style={styles.btnText}>Clicar</Text >
-        </>
+            <View style={styles.container_login}>
+                <Text
+                    style={styles.titulo_caixa_texto}>
+                    Login
+                </Text>
+                <TextInput
+                    style={styles.caixa_texto}
+                    onChangeText={(text) => { setEmail(text) }} />
+
+                <Text
+                    style={styles.titulo_caixa_texto}>
+                    Senha
+                </Text>
+                <TextInput
+                    style={styles.caixa_texto}
+                    onChangeText={(text) => { setSenha(text) }} />
+
+                <Pressable
+                    style={(state) => [styles.botao, state.pressed ? { opacity: 0.5 } : null]}
+                    onPress={logar}>
+                    <Text style={styles.desc_botao}>Entrar</Text>
+                </Pressable>
+            </View>
+        </View>
     );
-};
+}
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#007FFF',
+        backgroundColor: '#FFFACD'
+    },
+    container_login: {
+        flex: 2,
         alignItems: 'center'
     },
-    imagem: {
-        width: 100,
-        height: 100
+    titulo_caixa_texto: {
+        fontSize: 25,
+        color: 'black'
     },
-    btnText: {
-        width: 200,
-        height: 50,
-        alignItems: 'center',
-        marginTop: 10,
+    caixa_texto: {
+        width: '70%',
+        color: 'black',
+        borderWidth: 1,
+        borderRadius: 4,
+        margin: 3,
         backgroundColor: 'white'
+    },
+    botao: {
+        justifyContent: 'center',
+        backgroundColor: 'green',
+        paddingVertical: 10,
+        paddingHorizontal: 30,
+        marginTop: 20,
+        borderRadius: 10
+    },
+    desc_botao: {
+        fontSize: 20,
+        color: 'white'
+    },
+    painel_imagem: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    imagem: {
+        width: 200,
+        height: 200,
+        resizeMode: "center"
     }
+});
 
-})
-
-export default TelaLogin;
+export default Login;
